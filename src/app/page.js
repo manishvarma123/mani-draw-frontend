1bdd17c10c529a5d50b483e97946a66f27cba57b
manishvarma123 <manishvarma1302@gmail.com>
2024-11-07 22:54:11 +0530

    index page updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 'use client'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 const HomeMain = () => {
   const {showPage} = useSelector((state)=>state.route)
+  const router = useRouter();
+
+  useEffect(()=>{
+    if(showPage === "lucky-draw"){
+      router.push('/lucky-draw');
+    }
+  },[])
 
   return (
     <>
